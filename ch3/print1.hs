b7182903468c78b4aa12f951f8e136e51bddf71e
elias <elias.jm.garcia@gmail.com>
2018-12-17 15:25:59 -0600

    ch3 is done, yeet

---
+++ ch3/print1.hs
@@ -0,0 +1,5 @@
+module Print1 where
+
+main :: IO ()
+
+main = putStrLn "Enter the 36 Chambers of Lambda"
---
+++ ch3/print2.hs
@@ -0,0 +1,9 @@
+module Print2 where
+
+main :: IO ()
+
+main = do
+  putStrLn "Count to four for me:"
+  putStrLn "One Shaolin, two Shoalin"
+  putStrLn ", three Shaolin, and"
+  putStrLn " four!"
