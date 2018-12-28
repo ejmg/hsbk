5f0831079e231ffd0f77121f34b7619ba18f1970
elias <elias.jm.garcia@gmail.com>
2018-12-28 11:39:37 -0600

    by the powers of pattern matching, HOF, & case expressions combined...

---
+++ ch7/employee.hs
@@ -0,0 +1,26 @@
+module Employee where
+
+data Employee = Coder
+  | Manager
+  | VP
+  | CEO
+  deriving (Eq, Ord, Show)
+
+reportBoss :: Employee
+           -> Employee
+           -> IO ()
+reportBoss e e' =
+  putStrLn $ show e ++
+             " is the boss of " ++
+             show e'
+
+employeeRank :: Employee
+             -> Employee
+             -> IO ()
+
+employeeRank e e' =
+  case compare e e' of
+    GT -> reportBoss e e'
+    EQ -> putStrLn "Neither employee\
+                  \ is the boss."
+    LT -> (flip reportBoss) e e'
