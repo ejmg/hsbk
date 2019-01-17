921fcb90f9574ea21e3453c804ff036839dda9af
elias <elias.jm.garcia@gmail.com>
2019-01-16 18:01:43 -0600

    does not work, very evident that i can't think clearly rn

--- ch9/FearfulSymmetry.hs
+++ ch9/FearfulSymmetry.hs
@@ -14,7 +14,64 @@ myWords str = go str []
   where go str' ls
           | str' == "" = ls
           | startWithWord str' =
-            go (dropWhile (/=' ') str') ((takeWhile (/=' ') str') : ls)
-          | (not (startWithWord str')) =
+            go (dropWhile (/=' ') str') (ls ++ [(takeWhile (/=' ') str')])
+          | not $ startWithWord str' =
             go (dropWhile (==' ') str') ls
+          | otherwise = ls
+
+
+-- # 2:
+
+-- Similar to #1, but break on '\n' escape character.
+
+-- Given:
+
+-- module PoemLines where
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful\
+            \ symmetry?"
+sentences = firstSen ++ secondSen
+  ++ thirdSen ++ fourthSen
+
+shouldEqual =
+  [ "Tyger Tyger, burning bright"
+  , "In the forests of the night"
+  , "What immortal hand or eye"
+  , "Could frame thy fearful symmetry?"
+  ]
+
+main :: IO ()
+main =
+  print $
+  "Are they equal? "
+  ++ show (myLines sentences
+           == shouldEqual)
+
+
+-- Answer:
+
+myLines :: String -> [String]
+myLines sntnc = go sntnc []
+  where go sntnc' ls
+          | sntnc' == "" = ls          
+          | (dropWhile (/='\n') sntnc' /="") && ((head sntnc') /= '\n')=
+              go (dropWhile (/='\n') sntnc')
+              (ls ++ [(takeWhile (/='\n') sntnc')])
+          | (sntnc' /= "") && ((last sntnc') /= '\n') =
+              go "" (ls ++ [sntnc'])
+          | (sntnc' /= "") =
+              go (tail sntnc') ls
           | otherwise = []
+
+-- myLines sntnc = go sntnc []
+--   where go sntnc' ls
+--           | sntnc' == "" = ls
+--           | (dropWhile (/='\n') sntnc' /="") =
+--               go (dropWhile (/='\n') sntnc')
+--               (ls ++ [(takeWhile (/='\n') sntnc')])
+--           | (sntnc' /= "") =
+--               go "" (ls ++ [sntnc'])
+--           | otherwise = []
