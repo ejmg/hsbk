9f3384212504abfa8fafd6cf837d5ffcaf5a8383
elias <elias.jm.garcia@gmail.com>
2019-01-31 17:18:18 -0600

    finished ch8 exercises

--- ch8/ExercisesCh8.hs
+++ ch8/ExercisesCh8.hs
@@ -1,4 +1,6 @@
 module ExercisesCh8 where
+import Data.List (intersperse)
+
 
 -- Multiple Choice
 --------------------
@@ -61,4 +63,107 @@ frappe = flippy "haha"
 -- Recursion
 ---------------
 
+-- # 1:
 
+-- # 2:
+
+-- Answer
+rSum :: (Eq a, Num a) => a -> a
+rSum 0 = 0
+rSum n = n + rSum (n - 1)
+
+-- # 3:
+
+-- Answer: 
+rMult :: Integral a => a -> a -> a
+
+-- Better solution?
+rMult a b 
+  | (a == 0 || b == 0) = 0
+  | (a < b) = rMult b a
+  | b == 1 = a
+  | otherwise = a + rMult a (b - 1)
+
+-- Fixing dividedBy
+----------------------
+
+-- As provided in book, div function wasn't ideal bc didn't handle 0 or (-)
+-- values correctly, fix it.
+
+data DividedResult =
+    Result Integer
+  | DividedByZero deriving (Show)
+
+type Numerator = Integer
+type Denominator = Integer
+type Quotient = Integer
+
+dividedBy :: Numerator
+          -> Denominator
+          -> Quotient
+
+-- traditional division... yay?
+dividedBy = div
+
+-- recursive form
+
+dividedBy' :: Integer
+           -> Integer
+           -> DividedResult
+dividedBy' num denom = go absN absD 0
+  where mult = signum num * signum denom
+        absN = abs num
+        absD = abs denom
+        go n d count
+          | d == 0 = DividedByZero
+          | n == 0 = Result $ mult * count
+          | n < d = Result $ mult * count
+          | otherwise =
+              go (n - d) d (count + 1)
+
+-- McCarthy 91 function
+---------------------------
+
+mc91 :: Integer -> Integer
+mc91 n
+  | n <= 100 = mc91 . mc91 $ n + 11
+  | otherwise = n - 10
+
+-- Number into words
+-----------------------
+
+digitToWord :: Int -> String
+digitToWord n
+  | n == 0 = "zero"
+  | n == 1 = "one"
+  | n == 2 = "two"
+  | n == 3 = "three"
+  | n == 4 = "four"
+  | n == 5 = "five"
+  | n == 6 = "six"
+  | n == 7 = "seven"
+  | n == 8 = "eight"
+  | n == 9 = "nine"
+  | otherwise = "HMMM"
+
+oneDigit :: Int -> Bool
+oneDigit n = elem (div n 1) [0..9]
+
+digits :: Int -> [Int]
+digits n = go n []
+  where go n' ls
+          | (signum n') == (-1) =
+              go (abs n') ls
+          | oneDigit n' =
+            n' : ls
+          | otherwise =
+              go (div n' 10) (mod n' 10 : ls)
+
+-- solution is predicated on input not having *any* leading 0's.
+-- not sure how to make a cute but efficient solution to handle this
+-- without string interpolation or bit flipping.
+wordNumber :: Int -> String
+wordNumber n = concat
+               . intersperse "-"
+               $ map digitToWord
+               $ digits n
